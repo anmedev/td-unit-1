232d5460311e8bf183da28093e3f88ab3825098d
anmedev <aam2ds@virginia.edu>
2023-09-07 18:01:23 -0400

    refactor solutions

---
+++ arrow-functions-practice/convert.js
@@ -0,0 +1,29 @@
+// Conversions
+const addToTen = num => 10 + num;
+
+const divideUs = (num1, num2) => num1 / num2;
+
+const printMyName = () => {
+  let myName = 'Ashley';
+	console.log(myName);
+}
+
+console.log(addToTen(6));
+console.log(divideUs(16, 4));
+console.log(printMyName());
+
+
+
+// Original Code
+// function addToTen(num) {
+//   return 10 + num;
+// }
+
+// function divideUs(num1, num2) {
+//   return num1/num2;
+// }
+
+// function printMyName() {
+//   let myName = 'Ashley';
+// 	console.log(myName);
+// }
